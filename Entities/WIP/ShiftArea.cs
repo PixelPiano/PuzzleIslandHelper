7eecfc8c94d0c4207567d278c5f43db00070af39
PianoBoy <pianoboyedm@gmail.com>
2024-03-24 23:07:42 -0500

    Transition support
    Added support for when the player transitions between levels. Also now accurately culls tiles to lower cost of rendering.

--- Entities/WIP/ShiftArea.cs
+++ Entities/WIP/ShiftArea.cs
@@ -5,6 +5,10 @@ using System;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework.Graphics;
 using System.Collections;
+using Celeste.Mod.PuzzleIslandHelper.Effects;
+using System.Linq;
+using static Celeste.Mod.PuzzleIslandHelper.Triggers.SceneSwitch;
+using System.Security.Policy;
 
 namespace Celeste.Mod.PuzzleIslandHelper.Entities.WIP
 {
@@ -15,7 +19,6 @@ namespace Celeste.Mod.PuzzleIslandHelper.Entities.WIP
     {
         private char bgFrom, bgTo, fgFrom, fgTo;
         private int[] indices;
-        public static Dictionary<string, LevelTiles> TilesLookup => PianoModule.Session.TilesLookup;
         public struct LevelTiles
         {
             public TileGrid BgTiles;
@@ -26,7 +29,6 @@ namespace Celeste.Mod.PuzzleIslandHelper.Entities.WIP
         public VertexPositionColor[] Vertices;
         private Vector2 start;
         public bool UsesNodes;
-        private bool doNotUpdateVertices;
         public class VertexBreath : Component
         {
             public float Interval;
@@ -64,12 +66,14 @@ namespace Celeste.Mod.PuzzleIslandHelper.Entities.WIP
             }
         }
         public VertexBreath[] VertexBreathing;
+        public Vector2 origLevelOffset;
+        public Collider Box;
         public ShiftArea(EntityData data, Vector2 offset) : base(data.Position + offset)
         {
-            bgFrom = data.Char("bgFrom", ' ');
-            bgTo = data.Char("bgTo", ' ');
-            fgFrom = data.Char("fgFrom", ' ');
-            fgTo = data.Char("fgTo", ' ');
+            bgFrom = data.Char("bgFrom", '0');
+            bgTo = data.Char("bgTo", '0');
+            fgFrom = data.Char("fgFrom", '0');
+            fgTo = data.Char("fgTo", '0');
             string[] ind = data.Attr("indices").Replace(" ", "").Split(',');
             List<int> temp = new();
             foreach (string s in ind)
@@ -78,12 +82,10 @@ namespace Celeste.Mod.PuzzleIslandHelper.Entities.WIP
                 {
                     temp.Add(parsed);
                 }
-                else break;
             }
             indices = temp.ToArray();
             Depth = -10001;
             Collider = new Hitbox(data.Width, data.Height);
-            Tag |= Tags.TransitionUpdate;
             start = Position;
             Points = data.NodesWithPosition(Vector2.Zero);
             UsesNodes = Points is not null && Points.Length > 1;
@@ -94,65 +96,98 @@ namespace Celeste.Mod.PuzzleIslandHelper.Entities.WIP
                 Vertices[i] = new VertexPositionColor(new Vector3(Points[i], 0), Color.White);
                 VertexBreathing[i] = new VertexBreath(Calc.Random.Range(4, 8f), Calc.Random.Range(1f, 8));
             }
-            TransitionListener l = new TransitionListener();
-            l.OnOutBegin = () =>
-            {
-                doNotUpdateVertices = true;
-            };
-            Add(l);
+            Box = PianoUtils.Boundaries(Points, offset);
             Add(VertexBreathing);
-        }
-        public override void Update()
-        {
-            base.Update();
-            if (Scene is not Level level || doNotUpdateVertices) return;
-            for (int i = 0; i < Points.Length; i++)
-            {
-                Vector2 newPosition = Points[i] + (Position - start) - (level.Camera.Position - level.LevelOffset) - Vector2.UnitY * VertexBreathing[i].Amount;
-                Vertices[i].Position = new Vector3(newPosition.X, newPosition.Y - VertexBreathing[i].Amount, 0);
-            }
+            Tag |= Tags.TransitionUpdate;
         }
         public override void Awake(Scene scene)
         {
             base.Awake(scene);
-            SetUpTiles(scene);
+            Tiles = GetTiles(scene);
+            origLevelOffset = (scene as Level).LevelOffset;
+            UpdateVertices();
         }
-        public void SetUpTiles(Scene scene)
+        public override void Update()
         {
-            string name = (scene as Level).Session.Level;
-            if (TilesLookup.ContainsKey(name))
+            UpdateVertices();
+            base.Update();
+        }
+        public void UpdateVertices()
+        {
+            if (Scene is not Level level) return;
+            for (int i = 0; i < Points.Length; i++)
             {
-                Tiles = TilesLookup[name];
-            }
-            else
-            {
-                GetTiles(scene);
-                TilesLookup.Add(name, Tiles);
-            }
-            Add(Tiles.BgTiles, Tiles.FgTiles);
-            if (PianoUtils.SeekController<ShiftAreaRenderer>(scene) == null)
-            {
-                scene.Add(new ShiftAreaRenderer(true, Tiles.FgTiles));
-                scene.Add(new ShiftAreaRenderer(false, Tiles.BgTiles));
+                Vector2 entityDifference = Position - start;
+                Vector2 levelDifference = level.Camera.Position - origLevelOffset;
+                Vector2 newPosition = Points[i] + entityDifference - levelDifference;
+                Vertices[i].Position = new Vector3(newPosition.X, newPosition.Y - VertexBreathing[i].Amount, 0);
             }
         }
-        public void DrawMask()
+        public void RenderTiles(bool fg, Level level)
         {
-            GFX.DrawIndexedVertices(Matrix.Identity, Vertices, Vertices.Length, indices, indices.Length / 3);
-        }
-        public override void SceneEnd(Scene scene)
-        {
-            base.SceneEnd(scene);
-            TilesLookup.Clear();
-        }
-        public void GetTiles(Scene scene)
-        {
+            TileGrid grid = fg ? Tiles.FgTiles : Tiles.BgTiles;
+            if (grid is null || grid.Alpha <= 0f) return;
+            Vector2 offset = origLevelOffset - level.Camera.Position;
 
+            Rectangle clippedRenderTiles = GetClippedRenderTiles(grid, Box);
+            offset -= grid.VisualExtend * Vector2.One * 8;
+            int tileWidth = grid.TileWidth;
+            int tileHeight = grid.TileHeight;
+            Color color = grid.Color * grid.Alpha;
+            Vector2 position2 = new Vector2(offset.X + clippedRenderTiles.Left * tileWidth, offset.Y + clippedRenderTiles.Top * tileHeight);
+            position2 = position2.Round();
+            for (int i = clippedRenderTiles.Left; i < clippedRenderTiles.Right; i++)
+            {
+                for (int j = clippedRenderTiles.Top; j < clippedRenderTiles.Bottom; j++)
+                {
+                    MTexture mTexture = grid.Tiles[i, j];
+                    if (mTexture != null)
+                    {
+                        Draw.SpriteBatch.Draw(mTexture.Texture.Texture_Safe, position2, mTexture.ClipRect, color);
+                    }
+
+                    position2.Y += tileHeight;
+                }
+
+                position2.X += tileWidth;
+                position2.Y = offset.Y + clippedRenderTiles.Top * tileHeight;
+            }
+        }
+        public Rectangle GetClippedRenderTiles(TileGrid grid, Collider box)
+        {
+            int left, top, bottom, right;
+            Vector2 vector = box.AbsolutePosition - (box.AbsolutePosition - origLevelOffset) - grid.VisualExtend * Vector2.One * 8;;
+            int extend = grid.VisualExtend;
+            left = (int)Math.Max(0.0, Math.Floor((box.Left - vector.X) / grid.TileWidth) - extend);
+            top = (int)Math.Max(0.0, Math.Floor((box.Top - vector.Y) / grid.TileHeight) - extend);
+            right = (int)Math.Min(grid.TilesX, Math.Ceiling((box.Right - vector.X) / grid.TileWidth) + extend);
+            bottom = (int)Math.Min(grid.TilesY, Math.Ceiling((box.Bottom - vector.Y) / grid.TileHeight) + extend);
+
+            left = Math.Max(left, 0);
+            top = Math.Max(top, 0);
+            right = Math.Min(right, grid.TilesX);
+            bottom = Math.Min(bottom, grid.TilesY);
+
+            return new Rectangle(left, top, right - left, bottom - top);
+        }
+        public override void DebugRender(Camera camera)
+        {
+            base.DebugRender(camera);
+            Draw.HollowRect(Box, Color.Blue);
+        }
+        public void DrawMask(Matrix matrix)
+        {
+            GFX.DrawIndexedVertices(matrix, Vertices, Vertices.Length, indices, indices.Length / 3);
+        }
+        public LevelTiles GetTiles(Scene scene)
+        {
             Level level = scene as Level;
             int ox = (int)Math.Round((float)level.LevelSolidOffset.X);
             int oy = (int)Math.Round((float)level.LevelSolidOffset.Y);
             int tw = (int)Math.Ceiling(level.Bounds.Width / 8f);
             int th = (int)Math.Ceiling(level.Bounds.Height / 8f);
+
+            bool[] allowAir = { fgFrom == '0' || fgTo == '0', bgFrom == '0' || bgTo == '0' };
             if (fgFrom != fgTo)
             {
                 VirtualMap<char> fgData = level.SolidsData;
@@ -162,7 +197,7 @@ namespace Celeste.Mod.PuzzleIslandHelper.Entities.WIP
                 {
                     for (int y3 = oy - 1; y3 < oy + th + 1; y3++)
                     {
-                        if (x4 > 0 && x4 < fgTexes.Columns && y3 > 0 && y3 < fgTexes.Rows && fgData[x4, y3] != '0')
+                        if (x4 > 0 && x4 < fgTexes.Columns && y3 > 0 && y3 < fgTexes.Rows && (allowAir[0] || fgData[x4, y3] != '0'))
                         {
                             char c = fgData[x4, y3];
                             newFgData[x4 - ox + 1, y3 - oy + 1] = c == fgFrom ? fgTo : c == fgTo ? fgFrom : c;
@@ -184,7 +219,7 @@ namespace Celeste.Mod.PuzzleIslandHelper.Entities.WIP
                 {
                     for (int y = oy - 1; y < oy + th + 1; y++)
                     {
-                        if (x2 > 0 && x2 < bgTexes.Columns && y > 0 && y < bgTexes.Rows && bgData[x2, y] != '0')
+                        if (x2 > 0 && x2 < bgTexes.Columns && y > 0 && y < bgTexes.Rows && (allowAir[1] || bgData[x2, y] != '0'))
                         {
                             char c = bgData[x2, y];
                             newBgData[x2 - ox + 1, y - oy + 1] = c == bgFrom ? bgTo : c == bgTo ? bgFrom : c;
@@ -196,7 +231,7 @@ namespace Celeste.Mod.PuzzleIslandHelper.Entities.WIP
                 Tiles.BgTiles.VisualExtend = 1;
                 Tiles.BgTiles.Visible = false;
             }
+            return Tiles;
         }
-
     }
 }
--- Entities/WIP/ShiftAreaRenderer.cs
+++ Entities/WIP/ShiftAreaRenderer.cs
@@ -1,20 +1,12 @@
 using Celeste.Mod.Entities;
 using Microsoft.Xna.Framework;
 using Monocle;
-using Celeste.Mod.FancyTileEntities;
 using System;
-using System.Collections.Generic;
-using System.Collections;
 using Microsoft.Xna.Framework.Graphics;
-using System.Linq;
-using Celeste.Mod.PandorasBox;
-using Microsoft.Xna.Framework.Graphics.PackedVector;
-using System.Configuration;
-using FrostHelper.Helpers;
+using System.Collections.Generic;
 
 namespace Celeste.Mod.PuzzleIslandHelper.Entities.WIP
 {
-
     [Tracked]
     public class ShiftAreaRenderer : Entity
     {
@@ -24,92 +16,47 @@ namespace Celeste.Mod.PuzzleIslandHelper.Entities.WIP
         public static VirtualRenderTarget FGTarget => _FGTarget ??= VirtualContent.CreateRenderTarget("ShiftAreaFGTarget", 320, 180);
         public VirtualRenderTarget Target => FG ? FGTarget : BGTarget;
         public static bool UsedTemp;
-        public TileGrid Tiles;
         public bool FG;
-        public ShiftAreaRenderer(bool fg, TileGrid tiles) : base(Vector2.Zero)
+        private bool doNotRender;
+        public ShiftAreaRenderer(bool fg) : base(Vector2.Zero)
         {
             FG = fg;
             Depth = fg ? -10001 : 9999;
-            Tag |= Tags.TransitionUpdate;
+            Tag |= Tags.TransitionUpdate | Tags.Global;
             Add(new BeforeRenderHook(BeforeRender));
-            Tiles = tiles;
-        }
-        public void RenderAt(Vector2 offset)
-        {
-            if (Tiles.Alpha <= 0f) return;
-            Rectangle clippedRenderTiles = GetClippedRenderTiles(Tiles, offset);
-            int tileWidth = Tiles.TileWidth;
-            int tileHeight = Tiles.TileHeight;
-            Color color = Tiles.Color * Tiles.Alpha;
-            Vector2 position2 = new Vector2(offset.X + clippedRenderTiles.Left * tileWidth, offset.Y + clippedRenderTiles.Top * tileHeight);
-            position2 = position2.Round();
-            for (int i = clippedRenderTiles.Left; i < clippedRenderTiles.Right; i++)
-            {
-                for (int j = clippedRenderTiles.Top; j < clippedRenderTiles.Bottom; j++)
-                {
-                    MTexture mTexture = Tiles.Tiles[i, j];
-                    if (mTexture != null)
-                    {
-                        Draw.SpriteBatch.Draw(mTexture.Texture.Texture_Safe, position2, mTexture.ClipRect, color);
-                    }
-
-                    position2.Y += tileHeight;
-                }
-
-                position2.X += tileWidth;
-                position2.Y = offset.Y + clippedRenderTiles.Top * tileHeight;
-            }
-
-        }
-        public Rectangle GetClippedRenderTiles(TileGrid grid, Vector2 offset)
-        {
-            int left, top, bottom, right;
-
-            Level level = SceneAs<Level>();
-            Camera camera = level.Camera;
-            Vector2 vector = camera.Position + offset;
-            int extend = 0;//Tiles.VisualExtend;
-            left = (int)Math.Max(0.0, Math.Floor((camera.Left - vector.X) / grid.TileWidth) - extend);
-            top = (int)Math.Max(0.0, Math.Floor((camera.Top - vector.Y) / grid.TileHeight) - extend);
-            right = (int)Math.Min(grid.TilesX, Math.Ceiling((camera.Right - vector.X) / grid.TileWidth) + extend);
-            bottom = (int)Math.Min(grid.TilesY, Math.Ceiling((camera.Bottom - vector.Y) / grid.TileHeight) + extend);
-
-            left = Math.Max(left, 0);
-            top = Math.Max(top, 0);
-            right = Math.Min(right, grid.TilesX);
-            bottom = Math.Min(bottom, grid.TilesY);
-
-            return new Rectangle(left, top, right - left, bottom - top);
-        }
-        public static void Unload()
-        {
-            _FGTarget?.Dispose();
-            _BGTarget?.Dispose();
-            _FGTarget = null;
-            _BGTarget = null;
+            Collider = new Hitbox(320, 180);
         }
         public override void Render()
         {
             base.Render();
-            if (Scene is not Level level) return;
+            if (Scene is not Level level || doNotRender) return;
             UsedTemp = false;
             Draw.SpriteBatch.Draw(Target, level.Camera.Position, Color.White);
+        }
+        private bool levelHasAreas(Level level)
+        {
+            List<Entity> list = level.Tracker.GetEntities<ShiftArea>();
+            return !(list is null || list.Count <= 0);
         }
         public void BeforeRender()
         {
             if (Scene is not Level level) return;
+            doNotRender = !levelHasAreas(level);
+            Position = level.Camera.Position;
+            if(doNotRender) return;
+
             Matrix matrix = Matrix.Identity;
 
-            if (!UsedTemp)
+            if (!UsedTemp) //only draw masks once, since both renderers are running at the same time
             {
                 Engine.Graphics.GraphicsDevice.SetRenderTarget(GameplayBuffers.TempA);
                 Engine.Graphics.GraphicsDevice.Clear(Color.Transparent);
-                Draw.SpriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointWrap,
-                    DepthStencilState.None, RasterizerState.CullNone, null, matrix);
+
+                Draw.SpriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointWrap, DepthStencilState.None, RasterizerState.CullNone, null, matrix);
 
                 foreach (ShiftArea area in level.Tracker.GetEntities<ShiftArea>())
                 {
-                    area.DrawMask();
+                    area.DrawMask(matrix);
                 }
                 Draw.SpriteBatch.End();
                 UsedTemp = true;
@@ -118,19 +65,39 @@ namespace Celeste.Mod.PuzzleIslandHelper.Entities.WIP
 
             Engine.Graphics.GraphicsDevice.SetRenderTarget(Target);
             Engine.Graphics.GraphicsDevice.Clear(Color.Transparent);
-            Draw.SpriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointWrap,
-                DepthStencilState.None, RasterizerState.CullNone, null, matrix);
+            Draw.SpriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointWrap, DepthStencilState.None, RasterizerState.CullNone, null, matrix);
 
-            Vector2 offset = level.LevelOffset - level.Camera.Position;
-            RenderAt(offset - Tiles.VisualExtend * Vector2.One * 8);
+            foreach (ShiftArea area in level.Tracker.GetEntities<ShiftArea>())
+            {
+                Vector2 offset = area.origLevelOffset - level.Camera.Position;
+                area.RenderTiles(FG, level);
+            }
 
             Draw.SpriteBatch.End();
 
             Engine.Graphics.GraphicsDevice.SetRenderTarget(Target);
-            Draw.SpriteBatch.Begin(SpriteSortMode.Deferred, EasyRendering.AlphaMaskBlendState, SamplerState.PointClamp,
-                DepthStencilState.Default, RasterizerState.CullNone, null, Matrix.Identity);
+            Draw.SpriteBatch.Begin(SpriteSortMode.Deferred, EasyRendering.AlphaMaskBlendState, SamplerState.PointClamp, DepthStencilState.Default, RasterizerState.CullNone, null, Matrix.Identity);
             Draw.SpriteBatch.Draw(GameplayBuffers.TempA, Vector2.Zero, Color.White);
             Draw.SpriteBatch.End();
+        }
+        public static void Unload()
+        {
+            _FGTarget?.Dispose();
+            _BGTarget?.Dispose();
+            _FGTarget = null;
+            _BGTarget = null;
+            On.Celeste.LevelLoader.ctor -= LevelLoader_ctor;
+        }
+        public static void Load()
+        {
+            On.Celeste.LevelLoader.ctor += LevelLoader_ctor;
+        }
+
+        private static void LevelLoader_ctor(On.Celeste.LevelLoader.orig_ctor orig, LevelLoader self, Session session, Vector2? startPosition)
+        {
+            orig(self, session, startPosition);
+            self.Level.Add(new ShiftAreaRenderer(true));
+            self.Level.Add(new ShiftAreaRenderer(false));
         }
     }
 }
